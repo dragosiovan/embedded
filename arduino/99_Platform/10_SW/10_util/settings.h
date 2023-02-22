89ddfb6a9168922e329350ce6671d68aebcde2f5
Dragos <dragosiovan@yahoo.com>
2023-02-22 12:39:58 +0200

    [SW-UPD] Created platform folder - added general settings

---
+++ arduino/99_Platform/10_SW/10_util/settings.h
@@ -0,0 +1,12 @@
+// secret information: WiFi SSID and password, 
+// usernames, etc., to be used in arduino projects
+
+
+#define WIFI_SSID     ""
+#define WIFI_PASS     ""
+
+#define MQTT_SERVER      "192.168.0.222"
+#define MQTT_SERVERPORT  1883
+#define MQTT_USERNAME    ""
+#define MQTT_KEY         ""
+
